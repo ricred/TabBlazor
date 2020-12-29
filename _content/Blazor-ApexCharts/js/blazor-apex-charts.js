da290699a1309affccd09dac616085e1a5f2acee
joadan <joadan@users.noreply.github.com>
2020-12-29 14:31:51 +0000

    deploy: cdf0d72a78c113ff73cc3bbc6ca3a11aed5ef6b0

---
+++ _content/Blazor-ApexCharts/js/blazor-apex-charts.js
@@ -0,0 +1,48 @@
+﻿window.blazor_apexchart = {
+    charts: [],
+
+    destroyChart(chartId) {
+        var chartFind = this.charts.filter(x => x.opts.chart.chartId === chartId)
+        if (chartFind.length > 0) {
+            var chart = chartFind[0];
+            chart.destroy();
+            this.charts = this.charts.filter(x => x.opts.chart.chartId !== chartId);
+        }
+    },
+
+    renderChart(dotNetObject, container, options) {
+        var options = JSON.parse(options);
+
+        if (options.debug == true) {
+            console.log(options);
+        }
+
+        if (options.seriesNonXAxis != undefined) {
+
+            options.series = options.seriesNonXAxis;
+        }
+
+        options.chart.events = {
+            dataPointSelection: (event, chartContext, config) => {
+
+                var selection = {
+                    dataPointIndex: config.dataPointIndex,
+                    seriesIndex: config.seriesIndex,
+                    selectedDataPoints: config.selectedDataPoints
+                }
+
+                dotNetObject.invokeMethodAsync('DataPointSelected', selection);
+            }
+        }
+
+        //Always destry chart
+        this.destroyChart(options.chart.chartId);
+
+        chart = new ApexCharts(container, options);
+        this.charts.push(chart)
+        chart.render();
+        if (options.debug == true) {
+            console.log('Chart ' + options.chart.chartId + ' rendered');
+        }
+    }
+}
---
+++ _content/TabBlazor/js/tabblazor.js
@@ -0,0 +1,49 @@
+﻿window.blazorTabler = {
+
+    scrollToFragment: (elementId) => {
+        var element = document.getElementById(elementId);
+
+        if (element) {
+            element.scrollIntoView({
+                behavior: 'smooth'
+            });
+        }
+    },
+
+    showPrompt: (message, defaultValue) => {
+        return prompt(message, defaultValue);
+    },
+
+    showAlert: (message) => {
+        return alert(message);
+    },
+
+    windowOpen: (url, name, features, replace) => {
+        window.open(url, name, features, replace);
+        return "";
+    },
+
+    redirect: (url) => {
+        window.open(url);
+        return "";
+    },
+
+    setPropByElement: (element, property, value) => {
+        element[property] = value;
+        return "";
+    },
+
+
+    clickOutsideHandler: {
+        addEvent: function (elementId, dotnetHelper) {
+            window.addEventListener("click", (e) => {
+                var element = document.getElementById(elementId);
+                if (e != null && element != null) {
+                    if (e.target !== element && (!element.contains(e.target))) {
+                        dotnetHelper.invokeMethodAsync("InvokeClickOutside");
+                    }
+                }
+            });
+        }
+    }
+}
